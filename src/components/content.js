51ffbc9941b85d4ebac51d3435b2ace43c7a5cf7
Julia Athayde <jsilva@middlebury.edu>
2019-01-17 17:52:43 +0200

    initializing

--- src/components/content.js
+++ src/components/content.js
@@ -1,13 +1,23 @@
-/*
-  navigation-bar displays the list of sections for the collection of
-  articles passed down in its props. Clicking on one of these sections displays
-  a list of the available titles in that current section.
-*/
-/* eslint no-underscore-dangle: [2, { "allow": ["_id"] }] */
 import React from 'react';
-import classNames from 'classnames';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, Form, FormGroup,
-  Label, Input, CardImg, CardBody, CardGroup, CardSubtitle } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+  var icon = (
+    <span class="logo">
+      <a href="/">
+        <img src="/awesome-logo.png" height="33" width="120" alt="text here" /></a>
+    </span>
+  );
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -15,114 +25,54 @@ export default class NavBar extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      activeTab: '1'
+      isOpen: false
     };
   }
-
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
   render() {
     return (
       <div>
-        <Nav tabs>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
-            >
-              Intro & Participants
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
-            >
-              Why Swahili?
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '3' })}
-              onClick={() => { this.toggle('3'); }}
-            >
-              Why Kigali?
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '4' })}
-              onClick={() => { this.toggle('4'); }}
-            >
-              Swahili in Kigali
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('5'); }}
-            >
-              Swahili Resources
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: this.state.activeTab === '5' })}
-              onClick={() => { this.toggle('6'); }}
-            >
-              Contact Us
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="12">
-                <br/ >
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="3">
-            <Row>
-            </Row>
-          </TabPane>
-          <TabPane tabId="4">
-          </TabPane>
-          <TabPane tabId="5">
-          </TabPane>
-          <TabPane tabId="6">
-            <Row>
-              <Col sm="12">
-              <Form align = "left">
-                <FormGroup>
-                  <Label for="exampleName"><b>Name</b></Label>
-                  <Input type="email" name="user_name" id="exampleName" placeholder="Your name" />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="exampleEmail"><b>Email</b></Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="Insert your email here" />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="exampleText"><b>Ask Us A Question</b></Label>
-                  <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <Button> Submit </Button>
-              </Form>
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/" brand={icon}><b>KiK: Kiswahili in Kigali</b></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Menu
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Intro & Participants
+                </DropdownItem>
+                <DropdownItem>
+                  Why Swahili?
+                </DropdownItem>
+                <DropdownItem>
+                  Why Kigali?
+                </DropdownItem>
+                <DropdownItem>
+                  Swahili in Kigali
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="/components/">Swahili Resources</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="contact_us">Contact Us</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
