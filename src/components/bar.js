c96d66eb32a3d5b8464345ed9f568893503e40fc
Julia Athayde <jsilva@middlebury.edu>
2019-01-18 16:26:39 +0200

    fixed bug

--- src/components/bar.js
+++ src/components/bar.js
@@ -1,58 +1,78 @@
-/*
-  navigation-bar displays the list of sections for the collection of
-  articles passed down in its props. Clicking on one of these sections displays
-  a list of the available titles in that current section.
-*/
-/* eslint no-underscore-dangle: [2, { "allow": ["_id"] }] */
 import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+  var icon = (
+    <span class="logo">
+      <a href="/">
+        <img src="/awesome-logo.png" height="33" width="120" alt="text here" /></a>
+    </span>
+  );
 
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggle = this.toggle.bind(this);
     this.state = {
-      collapsed: true
+      isOpen: false
     };
   }
-
-  toggleNavbar() {
+  toggle() {
     this.setState({
-      collapsed: !this.state.collapsed
+      isOpen: !this.state.isOpen
     });
   }
   render() {
     return (
       <div>
-      <div>
-        <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">KiK</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/" brand={icon}><b>KiK: Kiswahili in Kigali</b></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Menu
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Intro & Participants
+                </DropdownItem>
+                <DropdownItem>
+                  Why Swahili?
+                </DropdownItem>
+                <DropdownItem>
+                  Why Kigali?
+                </DropdownItem>
+                <DropdownItem>
+                  Swahili in Kigali
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
               <NavItem>
-                <NavLink href="/intro/">Intro & Participants</NavLink>
+                <NavLink href="/components/">Swahili Resources</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/swahili/">Why Swahili?</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/kigali/">Why Kigali?</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/kik/">Swahili in Kigali</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/kik/">Swahili Resources</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/kik/">Contact Us</NavLink>
+                <NavLink href="contact_us">Contact Us</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
-      </div>
       </div>
     );
   }
