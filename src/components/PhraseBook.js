4836a5ca565b234fa41366c9e754c402e20d66bd
Julia Athayde <jsilva@middlebury.edu>
2019-01-21 14:05:05 +0200

    radio buttons

--- src/components/PhraseBook.js
+++ src/components/PhraseBook.js
@@ -1,63 +1,173 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import { Table, Button } from 'reactstrap';
-import Sound from 'react-sound';
+import ReactDOM from 'react-dom';
+
+
+const {render} = ReactDOM;
+
+const PhraseTable = ({selectedAudio, chooseSound}) =>
+  <Table hover>
+    <thead align='left'>
+      <tr>
+        <th>#</th>
+        <th>English</th>
+        <th>Tanzania</th>
+        <th>Burundi</th>
+        <th>DRC</th>
+        <th>Kenya</th>
+      </tr>
+    </thead>
+    <tbody align='left'>
+      <tr>
+        <th scope="row">1</th>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="green"
+                            checked={selectedAudio === 'green'}
+                            onChange={() => chooseSound('green')} />
+              I will return home soon.
+            </label>
+          </div>
+        </td>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="yellow"
+                            checked={selectedAudio === 'yellow'}
+                            onChange={() => chooseSound("yellow")} />
+              yellow
+            </label>
+          </div>
+        </td>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="pink"
+                            checked={selectedAudio === 'pink'}
+                            onChange={() => chooseSound("pink")} />
+              pink
+            </label>
+          </div>
+        </td>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="blue"
+                            checked={selectedAudio === 'blue'}
+                            onChange={() => chooseSound("blue")} />
+              blue
+            </label>
+          </div>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="green"
+                            checked={selectedAudio === 'green'}
+                            onChange={() => chooseSound('green')} />
+              They will put money on our account Thursday morning.
+            </label>
+          </div>
+        </td>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="yellow"
+                            checked={selectedAudio === 'yellow'}
+                            onChange={() => chooseSound("yellow")} />
+              yellow
+            </label>
+          </div>
+        </td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="green"
+                            checked={selectedAudio === 'green'}
+                            onChange={() => chooseSound('green')} />
+              I reached work late and my boss was upset.
+            </label>
+          </div>
+        </td>
+        <td>
+          <div className="radio">
+            <label>
+              <input type="radio" value="yellow"
+                            checked={selectedAudio === 'yellow'}
+                            onChange={() => chooseSound("yellow")} />
+              yellow
+            </label>
+          </div>
+        </td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </Table>
 
 export default class PhraseBook extends Component {
 
-  this.handleSong = this.handleSong.bind(this);
+  constructor(props) {
+    super(props);
+    this.state={ selectedAudio: "green"}
+    this.chooseSound = (color) => this.setState({selectedAudio: color});
+    this.playAudio = () => {
+      switch(this.state.selectedAudio){
+        case "green":
+          this.green.play();
+        case "blue":
+          this.blue.play();
+        case "pink":
+          this.pink.play();
+        case "yellow":
+          this.yellow.play();
+        default:
+          this.green.play();
+      }
+    }
+  }
 
   render() {
 
-    const listenButton = (
-      <Button color="secondary" onClick={this.handleSong}>Listen</Button>
-    );
-
-    const p1_English = (
-      <Sound
-        url=
-        volume= 100
-      />
-    )
 
     return (
-      <Table hover>
-        <thead align='left'>
-          <tr>
-            <th>#</th>
-            <th>English</th>
-            <th>Tanzania</th>
-            <th>Burundi</th>
-            <th>DRC</th>
-            <th>Kenya</th>
-          </tr>
-        </thead>
-        <tbody align='left'>
-          <tr>
-            <th scope="row">1</th>
-            <td>{listenButton} I will return home soon.</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>{listenButton} They will put money on our account Thursday morning.</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>{listenButton} I reached work late and my boss was upset.</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-            <td>{listenButton}</td>
-          </tr>
-        </tbody>
-      </Table>
+
+      <div className="stage">
+		<audio ref={(green) => { this.green = green; }}>
+			<source src="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" type="audio/mpeg" >
+			</source>
+		</audio>
+		<audio ref={(blue) => { this.blue = blue; }}>
+			<source src="https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"type="audio/mpeg" >
+			</source>
+		</audio>
+		<audio ref={(pink) => { this.pink = pink; }}>
+			<source src="https://s3.amazonaws.com/freecodecamp/simonSound3.mp3" type="audio/mpeg" >
+			</source>
+		</audio>
+		<audio ref={(yellow) => { this.yellow = yellow; }}>
+			<source src="https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" type="audio/mpeg" >
+			</source>
+		</audio>
+
+			<Button color="secondary" onClick={this.playAudio}>Listen</Button>
+			<PhraseTable selectedAudio={this.state.selectedAudio} chooseSound={this.chooseSound}/>
+      </div>
+
     );
   }
 }
+
+render(<PhraseBook />, document.getElementById('root'))
